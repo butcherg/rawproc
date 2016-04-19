d8b366e9177c5e9fba0a1506de8e05d1c4d80783
Glenn Butcher <Glenn.Butcher@gmail.com>
2016-04-18 20:06:03 -0600

    optimized 24bit

--- ThreadedConvolve.cpp
+++ ThreadedConvolve.cpp
@@ -24,6 +24,8 @@ ThreadedConvolve::~ThreadedConvolve()
 wxThread::ExitCode ThreadedConvolve::Entry()
 {
 	unsigned x, y;
+	BYTE * bsrcpix, * bdstpix;
+	FIRGB16 * wsrcpix, * wdstpix;
 	BYTE *bits = NULL;
 	
 	int bpp = FreeImage_GetBPP(src);
@@ -32,6 +34,11 @@ wxThread::ExitCode ThreadedConvolve::Entry()
 	unsigned pitch = FreeImage_GetPitch(src);
 	BYTE *dibbits = (BYTE*)FreeImage_GetBits(src);
 	//bits = dibbits +(pitch*(y))+(x*(bytespp));
+
+	unsigned spitch = FreeImage_GetPitch(src);
+	unsigned dpitch = FreeImage_GetPitch(dst);
+	void * srcbits = FreeImage_GetBits(src);
+	void * dstbits = FreeImage_GetBits(dst);
 
 	double R, G, B;
 	FIRGB16 value;
@@ -65,29 +72,24 @@ wxThread::ExitCode ThreadedConvolve::Entry()
 			break;
             
 		case 24 :
-
 			for(y = startrow+1; y < FreeImage_GetHeight(src)-1; y+=increment) {
-				//bits =  FreeImage_GetScanLine(src, y);
-				BYTE *srcbits = FreeImage_GetScanLine(src, y);
-				BYTE *dstbits = FreeImage_GetScanLine(dst, y);
 				for(x = 1; x < FreeImage_GetWidth(src)-1; x++) {
+					bdstpix = (BYTE *) dstbits + dpitch*y + 3*x;
 					R=0.0; G=0.0; B=0.0;
 					for (int kx=0; kx<3; kx++) {
-						for (int ky=0; ky<3; ky++) {
-							int ix = x-1+kx;
-							int iy = y-1+ky;
-							FreeImage_GetPixelColor16(src, ix, iy, &value);
-							//BYTE *pixel =  dibbits +(pitch*(iy))+(ix*(bytespp));
-							R += value.red   * kernel[kx][ky];
-							G += value.green * kernel[kx][ky];
-							B += value.blue  * kernel[kx][ky];
+						int ix=kx*3;
+						for (int ky=0; ky<3; ky++) { 
+							int i = ix+ky;
+							//BYTE *pixel = (BYTE *) srcbits + spitch*iy + 3*ix;
+							BYTE *pixel = (BYTE *) srcbits + spitch*(y-1+ky) + 3*(x-1+kx);
+							R += pixel[FI_RGBA_RED]   * kernel[kx][ky];
+							G += pixel[FI_RGBA_GREEN] * kernel[kx][ky];
+							B += pixel[FI_RGBA_BLUE]  * kernel[kx][ky];
 						}
+						bdstpix[FI_RGBA_RED]   = MIN(MAX(int(R), 0), 255);
+						bdstpix[FI_RGBA_GREEN] = MIN(MAX(int(G), 0), 255);
+						bdstpix[FI_RGBA_BLUE]  = MIN(MAX(int(B), 0), 255);
 					}
-					dstbits[FI_RGBA_RED]   = MIN(MAX(int(R), 0), 255);
-					dstbits[FI_RGBA_GREEN] = MIN(MAX(int(G), 0), 255);
-					dstbits[FI_RGBA_BLUE]  = MIN(MAX(int(B), 0), 255);
-					srcbits += 3;
-					dstbits += 3;
 				}
 			}
 			break;
