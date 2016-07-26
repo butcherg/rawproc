1dc1d021b577129a2c92bfae7e84465185ae62d0
Glenn Butcher <Glenn.Butcher@gmail.com>
2016-07-25 21:09:25 -0500

    added LUMALUT and NLMeans functions

--- FreeImage_Threaded.h
+++ FreeImage_Threaded.h
@@ -5,6 +5,7 @@
 
 double ApplyCurve(FIBITMAP *src, FIBITMAP *dst, std::vector<cp> ctpts, int threadcount);
 double ApplyLUT(FIBITMAP *src, FIBITMAP *dst, char * LUT, int threadcount);
+double ApplyLUT2LUMA(FIBITMAP *src, FIBITMAP *dst, char * LUT, int threadcount);
 double ApplyKernel(FIBITMAP *src, FIBITMAP *dst, double kernel[3][3], int threadcount);
 double ApplySaturation(FIBITMAP *src, FIBITMAP *dst, double saturate, int threadcount);
 double ApplyGray(FIBITMAP *src, FIBITMAP *dst, double redpct, double greenpct, double bluepct, int threadcount);
