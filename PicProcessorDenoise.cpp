d844ada60ff5b14d884332d219bd9bb3e296cbc2
Glenn Butcher <Glenn.Butcher@gmail.com>
2016-09-07 22:53:55 -0600

    add: local and patch sliders

--- PicProcessorDenoise.cpp
+++ PicProcessorDenoise.cpp
@@ -9,6 +9,14 @@
 #include "FreeImage_Threaded.h"
 #include <wx/fileconf.h>
 
+#define SIGMASLIDER 8000
+#define LOCALSLIDER 8001
+#define PATCHSLIDER 8002
+
+#define SIGMARESET 8010
+#define LOCALRESET 8011
+#define PATCHRESET 8012
+
 class DenoisePanel: public PicProcPanel
 {
 	public:
@@ -18,16 +26,39 @@ class DenoisePanel: public PicProcPanel
 			wxSizerFlags flags = wxSizerFlags().Center().Border(wxLEFT|wxRIGHT|wxTOP|wxBOTTOM);
 			wxArrayString p = split(params,",");
 			int initialvalue = atoi(p[0]);
+			int localval = atoi(p[1]);
+			int patchval = atoi(p[2]);
 
 			g->Add(0,10, wxGBPosition(0,0));
 			g->Add(new wxStaticText(this,wxID_ANY, "sigma: "), wxGBPosition(1,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			sigma = new wxSlider(this, wxID_ANY, initialvalue, 0, 100, wxPoint(10, 30), wxSize(140, -1));
+			sigma = new wxSlider(this, SIGMASLIDER, initialvalue, 0, 100, wxPoint(10, 30), wxSize(140, -1));
 			g->Add(sigma , wxGBPosition(1,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
 			val = new wxStaticText(this,wxID_ANY, p[0], wxDefaultPosition, wxSize(30, -1));
 			g->Add(val , wxGBPosition(1,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			btn = new wxBitmapButton(this, wxID_ANY, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
+			btn = new wxBitmapButton(this, SIGMARESET, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
 			btn->SetToolTip("Reset to default");
 			g->Add(btn, wxGBPosition(1,3), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+
+			g->Add(0,20, wxGBPosition(2,0));
+
+			g->Add(new wxStaticText(this,wxID_ANY, "local: "), wxGBPosition(3,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			local = new wxSlider(this, LOCALSLIDER, localval, 0, 15, wxPoint(10, 30), wxSize(140, -1));
+			g->Add(local , wxGBPosition(3,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			val1 = new wxStaticText(this,wxID_ANY, p[1], wxDefaultPosition, wxSize(30, -1));
+			g->Add(val1 , wxGBPosition(3,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			btn1 = new wxBitmapButton(this, LOCALRESET, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
+			btn1->SetToolTip("Reset to default");
+			g->Add(btn1, wxGBPosition(3,3), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+
+			g->Add(new wxStaticText(this,wxID_ANY, "patch: "), wxGBPosition(4,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			patch = new wxSlider(this, PATCHSLIDER, patchval, 0, 15, wxPoint(10, 30), wxSize(140, -1));
+			g->Add(patch , wxGBPosition(4,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			val2 = new wxStaticText(this,wxID_ANY, p[2], wxDefaultPosition, wxSize(30, -1));
+			g->Add(val2 , wxGBPosition(4,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+			btn2 = new wxBitmapButton(this, PATCHRESET, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
+			btn2->SetToolTip("Reset to default");
+			g->Add(btn2, wxGBPosition(4,3), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
+
 
 			SetSizerAndFit(g);
 			g->Layout();
@@ -48,44 +79,55 @@ class DenoisePanel: public PicProcPanel
 
 		void OnChanged(wxCommandEvent& event)
 		{
-			val->SetLabel(wxString::Format("%4d", sigma->GetValue()));
-			t->Start(500,wxTIMER_ONE_SHOT);
+			if (event.GetId() == SIGMASLIDER) val->SetLabel(wxString::Format("%4d", sigma->GetValue()));
+			if (event.GetId() == LOCALSLIDER) val1->SetLabel(wxString::Format("%4d", local->GetValue()));
+			if (event.GetId() == PATCHSLIDER) val2->SetLabel(wxString::Format("%4d", patch->GetValue()));
+			if (event.GetId() == SIGMASLIDER) t->Start(500,wxTIMER_ONE_SHOT);
 		}
 
 		void OnThumbTrack(wxCommandEvent& event)
 		{
-			val->SetLabel(wxString::Format("%4d", sigma->GetValue()));
+			if (event.GetId() == SIGMASLIDER) val->SetLabel(wxString::Format("%4d", sigma->GetValue()));
+			if (event.GetId() == LOCALSLIDER) val1->SetLabel(wxString::Format("%4d", local->GetValue()));
+			if (event.GetId() == PATCHSLIDER) val2->SetLabel(wxString::Format("%4d", patch->GetValue()));
 		}
 
 		void OnTimer(wxTimerEvent& event)
 		{
-			wxString local = wxConfigBase::Get()->Read("tool.denoise.local","3");
-			wxString patch = wxConfigBase::Get()->Read("tool.denoise.patch","1");
-
-
-			q->setParams(wxString::Format("%d,%s,%s",sigma->GetValue(),local,patch));
+			q->setParams(wxString::Format("%d,%d,%d",sigma->GetValue(),local->GetValue(),patch->GetValue()));
 			q->processPic();
 			event.Skip();
 		}
 
 		void OnButton(wxCommandEvent& event)
 		{
-			int resetval;
-			wxConfigBase::Get()->Read("tool.denoise.initialvalue",&resetval,0);
-			wxString local = wxConfigBase::Get()->Read("tool.denoise.local","3");
-			wxString patch = wxConfigBase::Get()->Read("tool.denoise.patch","1");
-			sigma->SetValue(resetval);
-			q->setParams(wxString::Format("%d,%s,%s",resetval,local,patch));
-			val->SetLabel(wxString::Format("%4d", resetval));
-			q->processPic();
+			int sigmareset, localreset, patchreset;
+			if (event.GetId() == SIGMARESET) {
+				wxConfigBase::Get()->Read("tool.denoise.initialvalue",&sigmareset,0);
+				sigma->SetValue(sigmareset);
+				val->SetLabel(wxString::Format("%4d", sigmareset));
+				q->setParams(wxString::Format("%d,%d,%d",sigma->GetValue(),local->GetValue(),patch->GetValue()));
+				q->processPic();
+			}
+			if (event.GetId() == LOCALRESET) {
+				wxConfigBase::Get()->Read("tool.denoise.local",&localreset,3);
+				local->SetValue(localreset);
+				val1->SetLabel(wxString::Format("%4d", localreset));
+			}
+			if (event.GetId() == PATCHRESET) {
+				wxConfigBase::Get()->Read("tool.denoise.patch",&patchreset,1);
+				patch->SetValue(patchreset);
+				val2->SetLabel(wxString::Format("%4d", patchreset));
+			}
+
 			event.Skip();
 		}
 
 
 	private:
-		wxSlider *sigma;
-		wxStaticText *val;
-		wxBitmapButton *btn;
+		wxSlider *sigma, *local, *patch;
+		wxStaticText *val, *val1, *val2;
+		wxBitmapButton *btn, *btn1, *btn2;
 		wxTimer *t;
 
 };
