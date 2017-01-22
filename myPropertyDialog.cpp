9ad2cbea925491fbd252b1ef8b56d42580b60ee2
Glenn Butcher <glenn.butcher@gmail.com>
2017-01-21 17:31:36 -0700

    property dialog filter

--- myPropertyDialog.cpp
+++ myPropertyDialog.cpp
@@ -7,14 +7,21 @@ PropertyDialog::PropertyDialog(wxWindow *parent, wxWindowID id, const wxString &
 wxDialog(parent, id, title, pos, size, wxRESIZE_BORDER)
 {
 	wxBoxSizer *sz = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer *ct = new wxBoxSizer(wxHORIZONTAL);
 	pg = new wxPropertyGrid(this, wxID_ANY);
 	SetExtraStyle(GetExtraStyle() & ~wxWS_EX_BLOCK_EVENTS);
 	for (std::map<std::string, std::string>::iterator it=props.begin(); it!=props.end(); ++it)
 		pg->Append(new wxStringProperty(it->first.c_str(), it->first.c_str(), it->second.c_str()));
 	pg->Sort();
 	sz->Add(pg, 0, wxEXPAND | wxALL, 3);
-	sz->Add(new wxButton(this, wxID_OK, "Dismiss"), 0, wxALL, 10);
+	
+	ct->Add(new wxButton(this, wxID_OK, "Dismiss"), 0, wxALL, 10);
+	ct->Add(new wxStaticText(this, wxID_ANY, "Filter: "), 0, wxALL, 10);
+	fil = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(100,25),wxTE_PROCESS_ENTER);
+	ct->Add(fil, 0, wxALL, 10);
+	sz->Add(ct, 0, wxALL, 10);
 	SetSizerAndFit(sz);
+	Bind(wxEVT_TEXT, &PropertyDialog::FilterGrid, this);
 }
 
 
@@ -22,6 +29,7 @@ PropertyDialog::PropertyDialog(wxWindow *parent, wxWindowID id, const wxString &
 wxDialog(parent, id, title, pos, size, wxRESIZE_BORDER)
 {
 	wxBoxSizer *sz = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer *ct = new wxBoxSizer(wxHORIZONTAL);
 	pg = new wxPropertyGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxPG_BOLD_MODIFIED | wxPG_HIDE_MARGIN);
 	SetExtraStyle(GetExtraStyle() & ~wxWS_EX_BLOCK_EVENTS);
 	wxString name, val; 
@@ -35,9 +43,15 @@ wxDialog(parent, id, title, pos, size, wxRESIZE_BORDER)
 
 	pg->Sort();
 	sz->Add(pg, 0, wxEXPAND | wxALL, 3);
-	sz->Add(new wxButton(this, wxID_OK, "Dismiss"), 0, wxALL, 10);
+
+	ct->Add(new wxButton(this, wxID_OK, "Dismiss"), 0, wxALL, 10);
+	ct->Add(new wxStaticText(this, wxID_ANY, "Filter: "), 0, wxALL, 10);
+	fil = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(100,25),wxTE_PROCESS_ENTER);
+	ct->Add(fil, 0, wxALL, 10);
+	sz->Add(ct, 0, wxALL, 10);
 	SetSizerAndFit(sz);
 	Bind(wxEVT_PG_CHANGED,&PropertyDialog::UpdateProperty,this);
+	Bind(wxEVT_TEXT, &PropertyDialog::FilterGrid, this);
 }
 
 
@@ -51,4 +65,28 @@ void PropertyDialog::UpdateProperty(wxPropertyGridEvent& event)
 	Refresh();
 	event.Skip();
 }
+
+void PropertyDialog::FilterGrid(wxCommandEvent& event)
+{
+	wxString str = fil->GetValue();
+	wxPropertyGridIterator it;
+	for ( it = pg->GetIterator();
+		!it.AtEnd();
+		it++ )
+	{
+    		wxPGProperty* p = *it;
+		if (str == "") {
+			p->Hide(false);
+		}
+		else {
+			if (p->GetName().Find(str) == wxNOT_FOUND) {
+				p->Hide(true);
+			}
+			else {
+				p->Hide(false);
+			}
+		}
+	}
+}
+
 
