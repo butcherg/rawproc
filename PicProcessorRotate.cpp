828a344d1a61f64c97452b91798f414575b6a665
Glenn Butcher <glenn.butcher@gmail.com>
2017-10-09 21:30:48 -0600

    fix: rotate autocrop, timer, other ui cleanup

--- PicProcessorRotate.cpp
+++ PicProcessorRotate.cpp
@@ -12,7 +12,7 @@
 class RotatePreview: public wxPanel
 {
 	public:
-		RotatePreview(wxWindow *parent, wxImage image, double angle, const wxSize &size=wxDefaultSize, const wxPoint &pos=wxDefaultPosition): wxPanel(parent, wxID_ANY, pos, size)
+		RotatePreview(wxWindow *parent, wxImage image, double angle, bool autocrop, const wxSize &size=wxDefaultSize, const wxPoint &pos=wxDefaultPosition): wxPanel(parent, wxID_ANY, pos, size)
 		{
 			SetDoubleBuffered(true);
 			haspect = (double) image.GetHeight() / (double) image.GetWidth();
@@ -20,7 +20,7 @@ class RotatePreview: public wxPanel
 			anglerad = angle * 0.01745329;
 			angledeg = angle;
 			orig = image;
-			autocrop = true;
+			this->autocrop = autocrop;
 
 			if (haspect < vaspect) {
 				img = image.Scale(size.GetWidth(), size.GetWidth()* haspect);
@@ -81,59 +81,78 @@ class RotatePreview: public wxPanel
 			//else 
 			//	img = orig.Scale(h * vaspect, h);
 
-			img = orig.Scale(w, h);
+			if (w>0 & h>0) img = orig.Scale(w, h);
 			event.Skip();
 			Refresh();
 		}
-
+		
 		void OnPaint(wxPaintEvent& event)
 		{
 			wxImage i;
-			int w, h, iw, ih;
+			int w, h, iw, ih, sw, sh;
+			float dx, dy;
 			GetSize(&w,&h);
-			if (haspect < vaspect)
-				i = img.Rotate(anglerad, wxPoint(img.GetWidth()/2,img.GetHeight()/2), true).Scale(w, w*haspect);
-			else
-				i = img.Rotate(anglerad, wxPoint(img.GetWidth()/2,img.GetHeight()/2), true).Scale(h*vaspect, h);
-			iw = i.GetWidth();
-			ih = i.GetHeight();
+			if (haspect < vaspect) {
+				i = img.Rotate(anglerad, wxPoint(img.GetWidth()/2,img.GetHeight()/2), true);
+				i = i.Scale(w, w*haspect);
+			}
+			else {
+				i = img.Rotate(anglerad, wxPoint(img.GetWidth()/2,img.GetHeight()/2), true);
+				i = i.Scale(h*vaspect, h);
+			}
+			sw = i.GetWidth();
+			sh = i.GetHeight();
 			wxPaintDC dc(this);
 			dc.DrawBitmap(wxBitmap(i),0,0);
-			dc.SetPen(wxPen(*wxYELLOW, 1, wxPENSTYLE_SHORT_DASH));
-			dc.DrawLine(0,ih*0.2,iw,ih*0.2);
-			dc.DrawLine(0,ih*0.4,iw,ih*0.4);
-			dc.DrawLine(0,ih*0.6,iw,ih*0.6);
-			dc.DrawLine(0,ih*0.8,iw,ih*0.8);
-			
-			dc.DrawLine(iw*0.2,0,iw*0.2,ih);
-			dc.DrawLine(iw*0.4,0,iw*0.4,ih);
-			dc.DrawLine(iw*0.6,0,iw*0.6,ih);
-			dc.DrawLine(iw*0.8,0,iw*0.8,ih);
-			
-			double a = angledeg / 45.0;
+ 
 
-//autocrop box, needs further work...			
-/*
-			int y1 = (ih*0.6) * a;
-			int y2 = ih - ((ih*0.4) * a);
-			int x1 = (iw*0.4) * a;
-			int x2 = iw - ((iw*0.6) * a);
-			
-			printf("%d,%d,%d,%d",x1, y1, x2, y2);
-			
-			dc.SetPen(wxPen(*wxYELLOW, 1, wxPENSTYLE_SOLID));
-			
-			dc.DrawLine(x1, y1, x2, y1);
-			dc.DrawLine(x2, y1, x2, y2);
-			dc.DrawLine(x2, y2, x1, y2);
-			dc.DrawLine(x1, y2, x1, y1);
-*/
+			dc.SetPen(wxPen(*wxLIGHT_GREY, 1, wxPENSTYLE_SHORT_DASH));
+			dc.DrawLine(0,h*0.2,w,h*0.2);
+			dc.DrawLine(0,h*0.4,w,h*0.4);
+			dc.DrawLine(0,h*0.6,w,h*0.6);
+			dc.DrawLine(0,h*0.8,w,h*0.8);
+
+			dc.DrawLine(w*0.2,0,w*0.2,h);
+			dc.DrawLine(w*0.4,0,w*0.4,h);
+			dc.DrawLine(w*0.6,0,w*0.6,h);
+			dc.DrawLine(w*0.8,0,w*0.8,h);
+
+			if (!autocrop) return;
+
+			int x1, y1;
+			if (angledeg > 0.0) {
+				for (x1=sw-1; x1>0; x1--) if ((byte) i.GetRed(x1,2) > 0) break;
+				for (y1=0; y1<sh; y1++) if ((byte) i.GetRed(2,y1) > 0) break;
+				if (x1 > sw/2 & y1 < sh/2) {
+					dc.SetPen(wxPen(*wxYELLOW, 1, wxPENSTYLE_SOLID));
+					dc.DrawLine(0, y1, sw, y1);
+					dc.DrawLine(0, sh-y1, sw, sh-y1);
+					dc.DrawLine(x1, 0, x1, sh);
+					dc.DrawLine(sw-x1, 0, sw-x1, sh);
+				}
+			}
+			else {
+				for (x1=0; x1<sw-1; x1++) if ((byte) i.GetRed(x1,2) > 0) break;
+				for (y1=sh-1; y1 > 0; y1--) if ((byte) i.GetRed(2,y1) > 0) break;
+				if (x1 < sw/2 & y1 > sh/2) {
+					dc.SetPen(wxPen(*wxYELLOW, 1, wxPENSTYLE_SOLID));
+					dc.DrawLine(0, y1, sw, y1);
+					dc.DrawLine(0, sh-y1, sw, sh-y1);
+					dc.DrawLine(x1, 0, x1, sh);
+					dc.DrawLine(sw-x1, 0, sw-x1, sh);
+				}
+			}
+		}
+		
+		void setAutocrop(bool a)
+		{
+			autocrop = a;
 		}
 
 	private:
 		wxImage img, orig;
 		double haspect, vaspect, aspect, anglerad, angledeg;
-		bool autocrop;
+		bool autocrop, grid;
 		cmsHTRANSFORM hTransform;
 
 };
@@ -143,19 +162,25 @@ class RotatePanel: public PicProcPanel
 	public:
 		RotatePanel(wxWindow *parent, PicProcessor *proc, wxString params): PicProcPanel(parent, proc, params)
 		{
+			bool acrop = false;
 			SetDoubleBuffered(true);
 			wxSize s = GetSize();
 			//SetSize(s);
 			wxSizerFlags flags = wxSizerFlags().Center().Border(wxLEFT|wxRIGHT|wxTOP|wxBOTTOM);
 			thumb = false;
-			double initialvalue = atof(params.c_str());
+			
+			wxArrayString tok = split(params, ",");
+			double initialvalue = atof(tok[0].c_str());
+			if (tok.GetCount() > 1)
+				if (tok[1] == "autocrop")
+					acrop = true;
 
 //with gridbagsizer:
 			//g->Add(0,10, wxGBPosition(0,0));
 			g->Add(new wxStaticText(this,wxID_ANY, "rotate: "), wxGBPosition(0,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 1);
 			rotate = new wxSlider(this, wxID_ANY, initialvalue*10.0, -450, 450, wxPoint(10, 30), wxSize(140, -1));
 			g->Add(rotate , wxGBPosition(0,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 1);
-			val = new wxStaticText(this,wxID_ANY, params, wxDefaultPosition, wxSize(30, -1));
+			val = new wxStaticText(this,wxID_ANY, tok[0], wxDefaultPosition, wxSize(30, -1));
 			g->Add(val , wxGBPosition(0,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 1);
 			btn1 = new wxBitmapButton(this, 8000, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
 			btn1->SetToolTip("Reset to default");
@@ -164,6 +189,10 @@ class RotatePanel: public PicProcPanel
 			//btn2->SetToolTip("Apply rotation");
 			//g->Add(btn2, wxGBPosition(0,4), wxDefaultSpan, wxALIGN_LEFT | wxALL, 1);
 			//g->Add(0,10, wxGBPosition(0,5), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT | wxALL, 1);
+			
+			autocrop = new wxCheckBox(this, wxID_ANY, "autocrop");
+			g->Add(autocrop , wxGBPosition(1,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 1);
+			autocrop->SetValue(acrop);
 
 			wxImage i = gImage2wxImage(proc->getPreviousPicProcessor()->getProcessedPic());
 			int pw = s.GetWidth();
@@ -173,28 +202,31 @@ class RotatePanel: public PicProcPanel
 			if (hTransform)
 				cmsDoTransform(hTransform, i.GetData(), i.GetData(), i.GetWidth()*i.GetHeight());
 
-			preview = new RotatePreview(this,i,initialvalue, wxSize(pw, ph));
+			preview = new RotatePreview(this,i,initialvalue, acrop, wxSize(pw, ph));
 			g->Add(preview , wxGBPosition(2,0), wxGBSpan(1,5), wxEXPAND | wxSHAPED | wxALIGN_LEFT |wxALIGN_TOP | wxALL, 1);
 
+			preview->setAutocrop(autocrop->GetValue());
+			
 			SetSizerAndFit(g);
 			g->Layout();
 
 			Refresh();
 			Update();
 			SetFocus();
-			//t = new wxTimer(this);
+			t = new wxTimer(this);
 			Bind(wxEVT_SIZE,&RotatePanel::OnSize, this);
 			Bind(wxEVT_BUTTON, &RotatePanel::OnButton, this);
 			Bind(wxEVT_SCROLL_CHANGED, &RotatePanel::OnChanged, this);
 			Bind(wxEVT_SCROLL_THUMBTRACK, &RotatePanel::OnThumbTrack, this);
 			Bind(wxEVT_SCROLL_THUMBRELEASE, &RotatePanel::OnThumbRelease, this);
+			Bind(wxEVT_CHECKBOX, &RotatePanel::OnChanged, this);
 			q->getCommandTree()->Bind(wxEVT_TREE_SEL_CHANGED, &RotatePanel::OnCommandtreeSelChanged, this);
-			//Bind(wxEVT_TIMER, &RotatePanel::OnTimer,  this);
+			Bind(wxEVT_TIMER, &RotatePanel::OnTimer,  this);
 		}
 
 		~RotatePanel()
 		{
-			//t->~wxTimer();
+			t->~wxTimer();
 			q->getCommandTree()-Unbind(wxEVT_TREE_SEL_CHANGED, &RotatePanel::OnCommandtreeSelChanged, this);
 		}
 
@@ -226,11 +258,12 @@ class RotatePanel: public PicProcPanel
 		{
 			//if (!thumb) {
 				val->SetLabel(wxString::Format("%2.1f", rotate->GetValue()/10.0));
+				preview->setAutocrop(autocrop->GetValue());
 				preview->Rotate(rotate->GetValue()/10.0);
-				//t->Start(500,wxTIMER_ONE_SHOT);
+				t->Start(500,wxTIMER_ONE_SHOT);
 				Refresh();
 				Update();
-				q->processPic();
+				//q->processPic();
 			//}
 		}
 
@@ -245,19 +278,25 @@ class RotatePanel: public PicProcPanel
 
 		void OnThumbRelease(wxCommandEvent& event)
 		{
-			q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
-			//q->processPic();
+			if (autocrop->GetValue())
+				q->setParams(wxString::Format("%2.1f,autocrop",rotate->GetValue()/10.0));
+			else
+				q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
+			q->processPic();
 			thumb = false;
 		}
 
-//		void OnTimer(wxTimerEvent& event)
-//		{
-//			q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
-//			q->processPic();
-//			DeletePendingEvents();
-//			t->Stop();
-//			event.Skip();
-//		}
+		void OnTimer(wxTimerEvent& event)
+		{
+			if (autocrop->GetValue())
+				q->setParams(wxString::Format("%2.1f,autocrop",rotate->GetValue()/10.0));
+			else
+				q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
+			q->processPic();
+			DeletePendingEvents();
+			t->Stop();
+			event.Skip();
+		}
 
 		void OnButton(wxCommandEvent& event)
 		{
@@ -266,12 +305,18 @@ class RotatePanel: public PicProcPanel
 				case 8000:
 					wxConfigBase::Get()->Read("tool.rotate.initialvalue",&resetval,0.0);
 					rotate->SetValue(resetval);
-					q->setParams(wxString::Format("%2.1f",resetval));
+					if (autocrop->GetValue())
+						q->setParams(wxString::Format("%2.1f,autocrop",rotate->GetValue()/10.0));
+					else
+						q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
 					val->SetLabel(wxString::Format("%2.1f", resetval));
 					preview->Rotate(0.0);
 					break;
 				case 9000:
-					q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
+					if (autocrop->GetValue())
+						q->setParams(wxString::Format("%2.1f,autocrop",rotate->GetValue()/10.0));
+					else
+						q->setParams(wxString::Format("%2.1f",rotate->GetValue()/10.0));
 					break;
 			}
 			Refresh();
@@ -281,11 +326,12 @@ class RotatePanel: public PicProcPanel
 
 
 	private:
+		wxCheckBox *autocrop;
 		wxSlider *rotate;
 		wxStaticText *val;
 		wxBitmapButton *btn1;
 		wxButton *btn2;
-		//wxTimer *t;
+		wxTimer *t;
 		RotatePreview *preview;
 		bool thumb;
 		cmsHTRANSFORM hTransform;
@@ -308,7 +354,14 @@ void PicProcessorRotate::createPanel(wxSimplebook* parent)
 
 bool PicProcessorRotate::processPic(bool processnext) {
 	((wxFrame*) m_display->GetParent())->SetStatusText("rotate...");
-	double angle = atof(c.c_str());
+	bool autocrop = false;
+	
+	wxArrayString t = split(c, ",");
+	
+	double angle = atof(t[0].c_str());
+	if (t.GetCount() > 1)
+		if (t[1] == "autocrop")
+			autocrop = true;
 	bool result = true;
 
 	int threadcount;
@@ -322,7 +375,7 @@ bool PicProcessorRotate::processPic(bool processnext) {
 	dib = new gImage(getPreviousPicProcessor()->getProcessedPic());
 	if (angle != 0.0) {
 		mark();
-		dib->ApplyRotate(-angle, false, threadcount);
+		dib->ApplyRotate(-angle, autocrop, threadcount);
 		wxString d = duration();
 
 		if ((wxConfigBase::Get()->Read("tool.all.log","0") == "1") || (wxConfigBase::Get()->Read("tool.rotate.log","0") == "1"))
