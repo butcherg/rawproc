715c67041c0cbf28f6d5810bead981783274eb81
Glenn Butcher <glenn.butcher@gmail.com>
2018-04-24 20:45:26 -0600

    lensfun cleanups

--- PicProcessorLensCorrection.cpp
+++ PicProcessorLensCorrection.cpp
@@ -185,24 +185,29 @@ class LensCorrectionPanel: public PicProcPanel
 			b->Add(new wxStaticText(this,-1, "lenscorrection", wxDefaultPosition, wxSize(100,20)), flags);
 			b->AddSpacer(3);
 
-			b->Add(new wxStaticText(this,-1, metadata, wxDefaultPosition, wxSize(260,40)), flags);
-			b->AddSpacer(3);
+			b->Add(new wxStaticText(this,-1, metadata, wxDefaultPosition, wxSize(260,50)), flags);
+			b->AddSpacer(5);
 
 			cam = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(200,25),wxTE_PROCESS_ENTER);
 			b->Add(cam, flags);
+			b->AddSpacer(2);
 			b->Add(new wxButton(this, CAMERAID, "Select camera"), flags);
 			b->AddSpacer(2);
 			lens = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(200,25),wxTE_PROCESS_ENTER);
 			b->Add(lens, flags);
+			b->AddSpacer(2);
 			b->Add(new wxButton(this, LENSID, "Select lens"), flags);
 			b->AddSpacer(3);
 			
 			ca = new wxCheckBox(this, wxID_ANY, "chromatic abberation");
 			b->Add(ca , flags);
+			b->AddSpacer(1);
 			vig = new wxCheckBox(this, wxID_ANY, "vignetting");
 			b->Add(vig , flags);
+			b->AddSpacer(1);
 			dist = new wxCheckBox(this, wxID_ANY, "distortion");
 			b->Add(dist , flags);
+			b->AddSpacer(1);
 			crop = new wxCheckBox(this, wxID_ANY, "autocrop");
 			b->Add(crop , flags);
 			
