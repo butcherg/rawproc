d09d0c4c9304c30e175078979db3f550772c2484
Glenn Butcher <glenn.butcher@gmail.com>
2018-06-22 08:52:29 -0600

    whitebalance work: float sliders

--- src/PicProcessorWhiteBalance.cpp
+++ src/PicProcessorWhiteBalance.cpp
@@ -65,165 +65,6 @@ class myFloatSlider: public wxSlider
 	
 };
 
-/*
-class WhiteBalancePanel: public PicProcPanel
-{
-
-	public:
-		WhiteBalancePanel(wxWindow *parent, PicProcessor *proc, wxString params): PicProcPanel(parent, proc, params)
-		{
-			double rm, gm, bm;
-
-			//parm tool.whitebalance.min: (float), minimum multiplier value.  Default=0.001
-			double min = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.min","0.001").c_str());
-			//parm tool.whitebalance.max: (float), maximum multiplier value.  Default=3.0
-			double max = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.max","3.0").c_str());
-			//parm tool.whitebalance.digits: (float), number of fractional digits.  Default=3
-			double digits = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.digits","3.0").c_str());
-			//parm tool.whitebalance.increment: (float), maximum multiplier value.  Default=0.001
-			double increment = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.increment","0.001").c_str());
-
-			wxSizerFlags flags = wxSizerFlags().Left().Border(wxLEFT|wxRIGHT).Expand();
-			wxArrayString p = split(params,",");
-			rm = atof(p[0]);
-			gm = atof(p[1]);
-			bm = atof(p[2]);
-
-			g->Add(new wxStaticText(this,wxID_ANY, "red mult:"), wxGBPosition(0,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			rmult = new wxSpinCtrlDouble(this, wxID_ANY,"1.0",wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxTE_PROCESS_ENTER);
-			rmult->SetDigits(digits);
-			rmult->SetRange(min,max);
-			rmult->SetIncrement(increment);
-			rmult->SetValue(rm);
-			g->Add(rmult, wxGBPosition(0,1), wxGBSpan(1,2), wxEXPAND | wxALIGN_LEFT |wxALL, 3);
-
-			g->Add(new wxStaticText(this,wxID_ANY, "green mult:"), wxGBPosition(1,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			gmult = new wxSpinCtrlDouble(this, wxID_ANY,"1.0",wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxTE_PROCESS_ENTER);
-			gmult->SetDigits(digits);
-			gmult->SetRange(min,max);
-			gmult->SetIncrement(increment);
-			gmult->SetValue(gm);
-			g->Add(gmult, wxGBPosition(1,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-
-			g->Add(new wxStaticText(this,wxID_ANY, "blue mult:"), wxGBPosition(2,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			bmult = new wxSpinCtrlDouble(this, wxID_ANY,"1.0",wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxTE_PROCESS_ENTER);
-			//bmult = new myDoubleCtrl(this, wxID_ANY);
-			bmult->SetDigits(digits);
-			bmult->SetRange(min,max);
-			bmult->SetIncrement(increment);
-			bmult->SetValue(bm);
-			g->Add(bmult, wxGBPosition(2,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-
-			g->Add(0,10, wxGBPosition(3,0));
-
-			g->Add(new wxButton(this, WBAUTO, "Auto"), wxGBPosition(4,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			g->Add(new wxButton(this, WBPATCH, "Patch"), wxGBPosition(5,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			
-			foo = new myFloatSlider(this, wxID_ANY, 1.0, 0.0, 2.0, 0.001);
-			bar = new wxStaticText(this,wxID_ANY, wxString::Format("%0.3f",1.0));
-			car = new wxStaticText(this,wxID_ANY, "-");
-			
-			g->Add(foo, wxGBPosition(6,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			g->Add(bar, wxGBPosition(6,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			g->Add(car, wxGBPosition(6,2), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			
-			Bind(wxEVT_SLIDER, WhiteBalancePanel::OnSlider, this);
-			
-			SetSizerAndFit(g);
-			g->Layout();
-			SetFocus();
-			t = new wxTimer(this);
-			Bind(wxEVT_SPINCTRLDOUBLE,&WhiteBalancePanel::paramChanged, this);
-			Bind(wxEVT_TIMER, &WhiteBalancePanel::OnTimer, this);
-			Bind(wxEVT_BUTTON, &WhiteBalancePanel::OnButton, this);
-			Bind(wxEVT_TEXT_ENTER, &WhiteBalancePanel::paramChanged, this);
-			Refresh();
-			Update();
-		}
-
-		~WhiteBalancePanel()
-		{
-			t->~wxTimer();
-		}
-		
-		void OnSlider(wxCommandEvent& event)
-		{
-			bar->SetLabel(wxString::Format("%0.3f",foo->GetFloatValue()));
-			car->SetLabel(wxString::Format("%d",foo->GetValue()));
-		}
-
-		void setMultipliers(double rm, double gm, double bm)
-		{
-			rmult->SetValue(rm);
-			gmult->SetValue(gm);
-			bmult->SetValue(bm);
-			Refresh();
-		}
-
-		void paramChanged(wxCommandEvent& event)
-		{
-			t->Start(500,wxTIMER_ONE_SHOT);
-		}
-
-		void OnTimer(wxTimerEvent& event)
-		{
-			q->setParams(wxString::Format("%f,%f,%f",rmult->GetValue(), gmult->GetValue(), bmult->GetValue()));
-			q->processPic();
-			event.Skip();
-		}
-
-		void OnButton(wxCommandEvent& event)
-		{
-			switch (event.GetId()) {
-				case WBAUTO:
-					q->setParams("");
-					q->processPic();
-					break;
-				case WBPATCH:
-					//parm tool.whitebalance.patchradius: (float), radius of patch.  Default=1.5
-					double radius = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.patchradius","1.5").c_str());
-
-					wxTextDataObject cb;
-					unsigned x, y;
-					double rm=1.0, gm=1.0, bm=1.0;
-					double ra, ga, ba;
-					if (wxTheClipboard->Open()) {
-						wxTheClipboard->GetData(cb);
-						wxArrayString p = split(cb.GetText(),",");
-
-						if (p.GetCount() == 2) {
-							x = atoi(p[0]);
-							y = atoi(p[1]);
-							std::vector<double> a = ((PicProcessorWhiteBalance *)q)->getPatchMeans(x, y, radius);
-							ra = a[0];
-							ga = a[1];
-							ba = a[2];
-							rm = ga / ra;
-							bm = ga / ba;
-							rmult->SetValue(rm);
-							gmult->SetValue(gm);
-							bmult->SetValue(bm);
-							q->setParams(wxString::Format("%f,%f,%f",rm, gm, bm));
-							q->processPic();
-							Refresh();
-						}
-						wxTheClipboard->Close();
-					}
-					break;
-			}
-			event.Skip();
-		}
-
-	private:
-		wxTextCtrl *edit;
-		wxSpinCtrlDouble *rmult, *gmult ,*bmult;
-		//myDoubleCtrl *bmult;
-		wxTimer *t;
-
-		myFloatSlider *foo;
-		wxStaticText *bar, *car;
-};
-*/
 
 class WhiteBalancePanel: public PicProcPanel
 {
@@ -237,10 +78,14 @@ class WhiteBalancePanel: public PicProcPanel
 			double min = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.min","0.001").c_str());
 			//parm tool.whitebalance.max: (float), maximum multiplier value.  Default=3.0
 			double max = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.max","3.0").c_str());
-			//parm tool.whitebalance.digits: (float), number of fractional digits.  Default=3
-			double digits = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.digits","3.0").c_str());
-			//parm tool.whitebalance.increment: (float), maximum multiplier value.  Default=0.001
+			//parm tool.whitebalance.digits: integer, number of fractional digits.  Default=3
+			unsigned digits = atoi(myConfig::getConfig().getValueOrDefault("tool.whitebalance.digits","3").c_str());
+			//parm tool.whitebalance.increment: (float), resolution of multiplier values.  Default=0.001
 			double increment = atof(myConfig::getConfig().getValueOrDefault("tool.whitebalance.increment","0.001").c_str());
+
+			nbrformat = "%0.";
+			nbrformat.Append(wxString::Format("%d",digits));
+			nbrformat.Append("f");
 
 			wxSizerFlags flags = wxSizerFlags().Left().Border(wxLEFT|wxRIGHT).Expand();
 			wxArrayString p = split(params,",");
@@ -249,53 +94,35 @@ class WhiteBalancePanel: public PicProcPanel
 			bm = atof(p[2]);
 
 			g->Add(new wxStaticText(this,wxID_ANY, "red:"), wxGBPosition(0,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			rmult = new myFloatSlider(this, WBRED, rm, 0.0, 2.0, 0.001);
-			rtxt  = new wxStaticText(this,wxID_ANY, wxString::Format("%0.3f",rm));
+			rmult = new myFloatSlider(this, WBRED, rm, 0.0, 2.0, 0.001, wxDefaultPosition, wxSize(100,20));
+			rtxt  = new wxStaticText(this,wxID_ANY, wxString::Format(nbrformat,rm));
 			g->Add(rmult, wxGBPosition(0,1), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
 			g->Add(rtxt, wxGBPosition(0,2), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
 
 			g->Add(new wxStaticText(this,wxID_ANY, "green:"), wxGBPosition(1,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			gmult = new wxSpinCtrlDouble(this, wxID_ANY,"1.0",wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxTE_PROCESS_ENTER);
-			gmult->SetDigits(digits);
-			gmult->SetRange(min,max);
-			gmult->SetIncrement(increment);
-			gmult->SetValue(gm);
-			g->Add(gmult, wxGBPosition(1,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
+			gmult = new myFloatSlider(this, WBGREEN, gm, 0.0, 2.0, 0.001, wxDefaultPosition, wxSize(100,20));
+			gtxt  = new wxStaticText(this,wxID_ANY, wxString::Format(nbrformat,gm));
+			g->Add(gmult, wxGBPosition(1,1), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
+			g->Add(gtxt, wxGBPosition(1,2), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
 
 			g->Add(new wxStaticText(this,wxID_ANY, "blue:"), wxGBPosition(2,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			bmult = new wxSpinCtrlDouble(this, wxID_ANY,"1.0",wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxTE_PROCESS_ENTER);
-			//bmult = new myDoubleCtrl(this, wxID_ANY);
-			bmult->SetDigits(digits);
-			bmult->SetRange(min,max);
-			bmult->SetIncrement(increment);
-			bmult->SetValue(bm);
-			g->Add(bmult, wxGBPosition(2,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
+			bmult = new myFloatSlider(this, WBBLUE, bm, 0.0, 2.0, 0.001, wxDefaultPosition, wxSize(100,20));
+			btxt  = new wxStaticText(this,wxID_ANY, wxString::Format(nbrformat,bm));
+			g->Add(bmult, wxGBPosition(2,1), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
+			g->Add(btxt, wxGBPosition(2,2), wxDefaultSpan, wxEXPAND | wxALIGN_LEFT |wxALL, 3);
 
 			g->Add(0,10, wxGBPosition(3,0));
 
 			g->Add(new wxButton(this, WBAUTO, "Auto"), wxGBPosition(4,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
 			g->Add(new wxButton(this, WBPATCH, "Patch"), wxGBPosition(5,0), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-
-			/*
-			foo = new myFloatSlider(this, wxID_ANY, 1.0, 0.0, 2.0, 0.001);
-			bar = new wxStaticText(this,wxID_ANY, wxString::Format("%0.3f",1.0));
-			car = new wxStaticText(this,wxID_ANY, "-");
-			
-			g->Add(foo, wxGBPosition(6,0), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			g->Add(bar, wxGBPosition(6,1), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			g->Add(car, wxGBPosition(6,2), wxDefaultSpan, wxALIGN_LEFT |wxALL, 3);
-			*/
-			
-			Bind(wxEVT_SLIDER, WhiteBalancePanel::OnSlider, this);
 			
 			SetSizerAndFit(g);
 			g->Layout();
 			SetFocus();
 			t = new wxTimer(this);
-			Bind(wxEVT_SPINCTRLDOUBLE,&WhiteBalancePanel::paramChanged, this);
+	
 			Bind(wxEVT_TIMER, &WhiteBalancePanel::OnTimer, this);
 			Bind(wxEVT_BUTTON, &WhiteBalancePanel::OnButton, this);
-			Bind(wxEVT_TEXT_ENTER, &WhiteBalancePanel::paramChanged, this);
 			Bind(wxEVT_SLIDER, &WhiteBalancePanel::paramChanged, this);
 			Refresh();
 			Update();
@@ -305,23 +132,15 @@ class WhiteBalancePanel: public PicProcPanel
 		{
 			t->~wxTimer();
 		}
-		
-		void OnSlider(wxCommandEvent& event)
-		{
-			//switch (event.GetId()) {
-				//case WBRED:
-					rtxt->SetLabel(wxString::Format("%0.3f",rmult->GetFloatValue()));
-					//break;
-			//}
-			event.Skip();
-		}
 
 		void setMultipliers(double rm, double gm, double bm)
 		{
 			rmult->SetFloatValue(rm);
-			rtxt->SetLabel(wxString::Format("%0.3f",rmult->GetFloatValue()));
-			gmult->SetValue(gm);
-			bmult->SetValue(bm);
+			rtxt->SetLabel(wxString::Format(nbrformat,rmult->GetFloatValue()));
+			gmult->SetFloatValue(gm);
+			gtxt->SetLabel(wxString::Format(nbrformat,gmult->GetFloatValue()));
+			bmult->SetFloatValue(bm);
+			btxt->SetLabel(wxString::Format(nbrformat,bmult->GetFloatValue()));
 			Refresh();
 		}
 
@@ -329,7 +148,13 @@ class WhiteBalancePanel: public PicProcPanel
 		{
 			switch (event.GetId()) {
 				case WBRED:
-					rtxt->SetLabel(wxString::Format("%0.3f",rmult->GetFloatValue()));
+					rtxt->SetLabel(wxString::Format(nbrformat,rmult->GetFloatValue()));
+					break;
+				case WBGREEN:
+					gtxt->SetLabel(wxString::Format(nbrformat,gmult->GetFloatValue()));
+					break;
+				case WBBLUE:
+					btxt->SetLabel(wxString::Format(nbrformat,bmult->GetFloatValue()));
 					break;
 			}
 			t->Start(500,wxTIMER_ONE_SHOT);
@@ -337,7 +162,7 @@ class WhiteBalancePanel: public PicProcPanel
 
 		void OnTimer(wxTimerEvent& event)
 		{
-			q->setParams(wxString::Format("%f,%f,%f",rmult->GetFloatValue(), gmult->GetValue(), bmult->GetValue()));
+			q->setParams(wxString::Format("%f,%f,%f",rmult->GetFloatValue(), gmult->GetFloatValue(), bmult->GetFloatValue()));
 			q->processPic();
 			event.Skip();
 		}
@@ -371,9 +196,11 @@ class WhiteBalancePanel: public PicProcPanel
 							rm = ga / ra;
 							bm = ga / ba;
 							rmult->SetFloatValue(rm);
-							rtxt->SetLabel(wxString::Format("%0.3f",rmult->GetFloatValue()));
-							gmult->SetValue(gm);
-							bmult->SetValue(bm);
+							rtxt->SetLabel(wxString::Format(nbrformat,rmult->GetFloatValue()));
+							gmult->SetFloatValue(gm);
+							gtxt->SetLabel(wxString::Format(nbrformat,gmult->GetFloatValue()));
+							bmult->SetFloatValue(bm);
+							btxt->SetLabel(wxString::Format(nbrformat,bmult->GetFloatValue()));
 							q->setParams(wxString::Format("%f,%f,%f",rm, gm, bm));
 							q->processPic();
 							Refresh();
@@ -387,13 +214,10 @@ class WhiteBalancePanel: public PicProcPanel
 
 	private:
 		wxTextCtrl *edit;
-		//wxSpinCtrlDouble *rmult, 
-		wxSpinCtrlDouble *gmult ,*bmult;
-		myFloatSlider *rmult; //, *gmult ,*bmult;
+		myFloatSlider *rmult, *gmult ,*bmult;
 		wxTimer *t;
-
-		myFloatSlider *foo;
 		wxStaticText *rtxt, *gtxt, *btxt, *bar, *car;
+		wxString nbrformat;
 };
 
 PicProcessorWhiteBalance::PicProcessorWhiteBalance(wxString name, wxString command, wxTreeCtrl *tree, PicPanel *display): PicProcessor(name, command, tree, display) 
