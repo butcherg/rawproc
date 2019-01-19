adedf351d5a6fcbf4bfce673e128c3195ab112f6
Glenn Butcher <glenn.butcher@gmail.com>
2019-01-19 10:42:36 -0700

    fix: GBRG pattern in applyDemosic()

--- src/gimage.cpp
+++ src/gimage.cpp
@@ -2166,7 +2166,7 @@ void gImage::ApplyDemosaic(GIMAGE_DEMOSAIC algorithm, int threadcount)
 
 	std::vector<unsigned> q = {0, 1, 1, 2};  //default pattern is RGGB, where R=0, G=1, B=2
 	if (imginfo["LibrawCFAPattern"] == "GRBG") q = {1, 0, 2, 1};
-	if (imginfo["LibrawCFAPattern"] == "GBRG") q = {1, 1, 0, 1};
+	if (imginfo["LibrawCFAPattern"] == "GBRG") q = {1, 2, 0, 1};
 	if (imginfo["LibrawCFAPattern"] == "BGGR") q = {2, 1, 1, 0};
 
 	if (algorithm == DEMOSAIC_HALF | algorithm == DEMOSAIC_HALF_RESIZE) {
