056b96bfe722b82d4a5ad96d32848350950296fa
Glenn Butcher <glenn.g.butcher@boeing.com>
2019-02-25 15:55:41 -0700

    refined EV display in histogram, still in work

--- src/myHistogramPane.cpp
+++ src/myHistogramPane.cpp
@@ -22,8 +22,6 @@ myHistogramPane::myHistogramPane(wxWindow* parent, const wxPoint &pos, const wxS
 	hscale = 0;
 	ord = 1;
 	EVaxis = false;
-	//parm histogram.ev.zero: Tone in the 0.0-1.0 scale to plot as EV0.  Default: 0.18
-	EV0 = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.zero","0.18").c_str());
 	
 	r = NULL; g = NULL; b = NULL;
 	rlen=0; glen=0; blen=0;
@@ -32,24 +30,6 @@ myHistogramPane::myHistogramPane(wxWindow* parent, const wxPoint &pos, const wxS
 	MouseX = 0; MouseY=0;
 	pressedDown = false;
 	inwindow = false;
-
-	int fontsize = atoi(myConfig::getConfig().getValueOrDefault("app.parameters.fontsize","10").c_str());
-	wxString fc = wxString(myConfig::getConfig().getValueOrDefault("app.parameters.fontcolor","0"));
-	if (fc == "") fc = "0";
-	wxArrayString fntc = split(fc,",");
-	fr = atoi(fntc[0].c_str());
-	if (fntc.GetCount() < 3) {
-		fg = atoi(fntc[0].c_str());
-		fb = atoi(fntc[0].c_str());
-	}
-	else if (fntc.GetCount() == 3) {
-		fg = atoi(fntc[1].c_str());
-		fb = atoi(fntc[2].c_str());
-	}
-
-	SetForegroundColour(wxColour(fr,fg,fb));
-	wxFont font(wxFontInfo(fontsize).Family(wxFONTFAMILY_SWISS));
-	SetFont(font);
 
 	Bind(wxEVT_MOTION, &myHistogramPane::mouseMoved, this);
 	Bind(wxEVT_LEFT_DOWN, &myHistogramPane::mouseDown, this);
@@ -82,7 +62,6 @@ myHistogramPane::myHistogramPane(wxWindow* parent, gImage &dib, const wxPoint &p
 	hscale = 0;
 	ord = 1;
 	EVaxis = false;
-	EV0 = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.zero","0.18").c_str());
 	display_channels = CHANNEL_RGB;
 
 	//not needed, for now; renders 'c' key command inop
@@ -102,7 +81,6 @@ myHistogramPane::myHistogramPane(wxWindow* parent, gImage &dib, const wxPoint &p
 	g = new wxPoint[glen];
 	for (unsigned i=0; i<rlen; i++) g[i] = wxPoint(i,gdata[i]);
 
-
 	bdata = dib.Histogram(CHANNEL_BLUE, hm);
 	if (hmax < hm) hmax = hm;
 	if (hscale < bdata.size()) hscale = bdata.size();
@@ -114,6 +92,7 @@ myHistogramPane::myHistogramPane(wxWindow* parent, gImage &dib, const wxPoint &p
 	SetInitialSize(wxSize(500,400));
 	pressedDown = false;
 	inwindow = false;
+
 
 	Bind(wxEVT_MOTION, &myHistogramPane::mouseMoved, this);
 	Bind(wxEVT_LEFT_DOWN, &myHistogramPane::mouseDown, this);
@@ -225,13 +204,38 @@ void myHistogramPane::render(wxDC&  dc)
 	GetSize(&w, &h);
 	w-=6;
 	h-=6;
-	//dc.SetBackground(*wxWHITE_BRUSH);
+
 	dc.Clear();
 	if (blankpic) return;
-	
+
+	//set up colors:
+	int fontsize = atoi(myConfig::getConfig().getValueOrDefault("app.parameters.fontsize","10").c_str());
+	dc.SetTextForeground(wxString2wxColour(wxString(myConfig::getConfig().getValueOrDefault("app.parameters.fontcolor","0"))));
+	wxFont font(wxFontInfo(fontsize).Family(wxFONTFAMILY_SWISS));
+	dc.SetFont(font);
+
+	//parm app.parameters.linecolor: integer RGB triplet or single of 0-255 (e.g., 128,128,128 or just 128 for gray) to specify default  color to be used to draw lines in the parameter pane. Some tools override this, look for "linecolor" in the Properties filter. Default=0
+	wxString applinecolorstring = wxString(myConfig::getConfig().getValueOrDefault("app.parameters.linecolor","0"));
+
+	//parm histogram.ev.linecolor: integer RGB triplet or single of 0-255 (e.g., 128,128,128 or just 128 for gray) to specify default  color to be used to draw EV lines in the parameter pane.  Default=app.parameters.linecolor
+	wxColour linecolor = wxString2wxColour(wxString(myConfig::getConfig().getValueOrDefault("histogram.ev.linecolor",applinecolorstring.ToStdString())));
+
+	//parm histogram.cursor.linecolor: integer RGB triplet or single of 0-255 (e.g., 128,128,128 or just 128 for gray) to specify default  color to be used to draw the histogram cursor. Some tools override this, look for "linecolor" in the Properties filter. Default=app.parameters.linecolor
+	wxColour cursorcolor = wxString2wxColour(wxString(myConfig::getConfig().getValueOrDefault("histogram.cursor.linecolor",applinecolorstring.ToStdString())));
+
+
+	int lineheight = wxSize(dc.GetTextExtent("l")).GetHeight();
+
+
 	//go to histogram coordinates:
-	dc.SetLogicalScale(((double) w / (double) hscale)* wscale, ((double) h/ (double) hmax) * wscale);
-	dc.SetDeviceOrigin (xorigin, h-yorigin);
+	if (EVaxis) {
+		dc.SetLogicalScale(((double) w / (double) hscale)* wscale, ((double) (h-lineheight)/ (double) hmax) * wscale);
+		dc.SetDeviceOrigin (xorigin, h-yorigin-lineheight);
+	}
+	else {
+		dc.SetLogicalScale(((double) w / (double) hscale)* wscale, ((double) h/ (double) hmax) * wscale);
+		dc.SetDeviceOrigin (xorigin, h-yorigin);
+	}
 	dc.SetAxisOrientation(true,true);
 
 	wxPoint * frontcolor;
@@ -269,18 +273,22 @@ void myHistogramPane::render(wxDC&  dc)
 		if (order>3) order=1;
 	}
 
-	if (EVaxis) {
-		EV0 = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.zero","0.18").c_str());
-		//parm histogram.ev.range: +/- range to plot EV.  Default: 3.0 (-/+ 3 stops)
-		float EVrange = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.range","3.0").c_str());
-		//parm histogram.ev.increment: Step increment for EV plot.  Default: 1.0
-		float EVinc = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.increment","1.0").c_str());
+	//parm histogram.ev.zero: Tone in the 0.0-1.0 scale to plot as EV0.  Default: 0.18
+	EV0 = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.zero","0.18").c_str());
+	//parm histogram.ev.range: +/- range to plot EV.  Default: 3.0 (-/+ 3 stops)
+	float EVrange = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.range","3.0").c_str());
+	//parm histogram.ev.increment: Step increment for EV plot.  Default: 1.0
+	float EVinc = atof(myConfig::getConfig().getValueOrDefault("histogram.ev.increment","1.0").c_str());
 
-		dc.SetPen(wxPen(wxColour(192,192,192), 1, wxPENSTYLE_SOLID ));
+	if (EVaxis) {
+		dc.SetPen(wxPen(linecolor, 1, wxPENSTYLE_SOLID ));
 		dc.DrawLine(hscale * EV0, 0, hscale * EV0, hmax);
-		dc.SetPen(wxPen(wxColour(192,192,192), 1, wxPENSTYLE_LONG_DASH ));
-		for (float ev=-EVrange; ev<=EVrange; ev+=EVinc)
+		dc.SetPen(wxPen(linecolor, 1, wxPENSTYLE_LONG_DASH ));
+		for (float ev=-EVrange; ev<=EVrange; ev+=EVinc) {
 			dc.DrawLine(hscale * EV0*pow(2.0, ev), 0, hscale * EV0*pow(2.0, ev), hmax);
+
+
+		}
 
 	}
 
@@ -292,12 +300,12 @@ void myHistogramPane::render(wxDC&  dc)
 */
 
 	//marker lines:
-	dc.SetPen(wxPen(wxColour(192,192,192),1));
+	dc.SetPen(wxPen(cursorcolor));
 	int mlx = dc.DeviceToLogicalX(wxCoord(MouseX));
 	float mlx_ev = hscale * mlx*pow(2.9, 1.0/mlx);
 	
 	unsigned mly=0;
-	//if (mlx > 0 & mlx < hscale) {
+	//if (mlx > 0 & mlx < hscale) { 
 	//	mly = wxCoord(frontcolor[mlx].y);
 	//	dc.DrawLine(0,mly,hscale,mly);
 	//}
@@ -308,6 +316,8 @@ void myHistogramPane::render(wxDC&  dc)
 	dc.SetAxisOrientation(true,false);
 
 	dc.SetTextBackground(wxColour(255,255,255));
+
+	dc.SetPen(wxPen(cursorcolor));
 	if (inwindow & !pressedDown & mlx > 0 & mlx < hscale) 
 		dc.DrawLine(MouseX,0,MouseX,h);
 	if (mlx < 0) mlx = 0;
@@ -324,8 +334,22 @@ void myHistogramPane::render(wxDC&  dc)
 	if (EVaxis) {
 		str1 = wxString::Format("ev0: %0.2f",EV0);
 		wxSize sz1 = dc.GetTextExtent(str1);
-		dc.DrawText(str1,w-sz1.GetWidth()-3,20);
+		dc.DrawText(str1,w-sz1.GetWidth()-3,lineheight+4);
+		for (float ev=-EVrange; ev<=EVrange; ev+=EVinc) {
+			wxString e; float fpart, ipart;
+			fpart = modf(ev, &ipart);
+			if (abs(fpart) > 0.0) 
+				e = wxString::Format("%1.1f", abs(ev));
+			else
+				e = wxString::Format("%1.0f", abs(ev));
+			int ew = wxSize(dc.GetTextExtent(e)).GetWidth();
+			if (ev < 0.0) e.Prepend("-");
+			if (ev > 0.0) e.Prepend("+");
+			ew = wxSize(dc.GetTextExtent(e)).GetWidth() - ew;
+			dc.DrawText(e,((int) ((w*EV0*pow(2.0, ev)*wscale)-ew))+xorigin,(h-(lineheight-1))-yorigin);
+		}
 	}
+
 }
 
 
