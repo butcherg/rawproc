a0e6bf1e980bfa064e3284b75f4d163456876b04
Glenn Butcher <glenn.butcher@gmail.com>
2019-03-04 20:41:32 -0700

    exposure work

--- src/PicProcessorExposure.cpp
+++ src/PicProcessorExposure.cpp
@@ -1,11 +1,14 @@
 #include "PicProcessor.h"
 #include "PicProcessorExposure.h"
 #include "PicProcPanel.h"
+#include "myRowColumnSizer.h"
 #include "myConfig.h"
 #include "util.h"
 #include "undo.xpm"
 
-#define EXPOSUREENABLE 7000
+#define EXPOSUREENABLE	 7000
+#define EXPOSUREEV	 7001
+#define EXPOSURETARGETEV 7002
 
 class ExposurePanel: public PicProcPanel
 {
@@ -17,22 +20,36 @@ class ExposurePanel: public PicProcPanel
 
 			double initialvalue = atof(params.c_str());
 
-			enablebox = new wxCheckBox(this, EXPOSUREENABLE, "exposure compensation:");
+			enablebox = new wxCheckBox(this, EXPOSUREENABLE, "exposure:");
 			enablebox->SetValue(true);
-			g->Add(enablebox, wxGBPosition(0,0), wxGBSpan(1,3), wxALIGN_LEFT | wxALL, 3);
-			g->Add(new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxSize(200,2)),  wxGBPosition(1,0), wxGBSpan(1,4), wxALIGN_LEFT | wxBOTTOM | wxEXPAND, 10);
 
+			evb = new wxRadioButton(this, EXPOSUREEV, "compensation", wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
+			evtgtb = new wxRadioButton(this, EXPOSURETARGETEV, "target patch");
 
 			ev = new wxSlider(this, wxID_ANY, 50.0+(initialvalue*10.0), 0, 100, wxPoint(10, 30), wxSize(140, -1));
-			g->Add(ev, wxGBPosition(2,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
 			val = new wxStaticText(this,wxID_ANY, wxString::Format("%2.2f", initialvalue), wxDefaultPosition, wxSize(30, -1));
-			g->Add(val , wxGBPosition(2,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
 			btn = new wxBitmapButton(this, wxID_ANY, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
 			btn->SetToolTip("Reset to default");
-			g->Add(btn, wxGBPosition(2,3), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
 
-			SetSizerAndFit(g);
-			g->Layout();
+
+			myRowColumnSizer *m = new myRowColumnSizer(10,3);
+			m->AddItem(enablebox, wxALIGN_LEFT);
+			m->NextRow();
+			m->AddItem(new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxSize(280,2)), wxALIGN_LEFT, 2);
+			m->NextRow();
+			m->AddItem(evb, wxALIGN_LEFT);
+			m->NextRow();
+			m->AddItem(ev, wxALIGN_LEFT);
+			m->AddItem(val, wxALIGN_LEFT);
+			m->AddItem(btn, wxALIGN_LEFT);
+			m->NextRow();
+			m->AddItem(new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxSize(280,2)), wxALIGN_LEFT, 2);
+			m->NextRow();
+			m->AddItem(evtgtb, wxALIGN_LEFT);
+			m->NextRow();
+			SetSizerAndFit(m);
+			m->Layout();
+
 			Refresh();
 			Update();
 			SetFocus();
@@ -100,6 +117,7 @@ class ExposurePanel: public PicProcPanel
 		wxStaticText *val;
 		wxBitmapButton *btn;
 		wxCheckBox *enablebox;
+		wxRadioButton *evb, *evtgtb;
 		wxTimer *t;
 
 };
@@ -154,7 +172,48 @@ void PicProcessorExposure::createPanel(wxSimplebook* parent)
 
 bool PicProcessorExposure::processPic(bool processnext) 
 {
-	double ev = atof(c.c_str());
+	double ev;
+	int x=0, y=0;
+	float radius=0.0;
+	bool expv = true;
+
+	//params: 
+	//	either: 
+	//		ev
+	//	or:
+	//		patch=x,y;radius=r;ev0=e
+	//	or:
+	//		ev=e
+	if (c.find("=") == std::string::npos) {
+		ev = atof(c.c_str());
+	}
+	else {
+		wxArrayString p = split(wxString(c),";");
+		for (int i=0; i<p.GetCount(); i++) {
+			wxArrayString nv = split(p[i],"=");
+			if (nv.GetCount() == 2) {
+				if (nv[0] == "patch") {
+					wxArrayString xy = split(nv[1],",");
+					if (xy.GetCount() == 2) {
+						x = atoi(xy[0].mb_str());
+						y = atoi(xy[1].mb_str());
+					}
+					else return false;
+				}
+				else if (nv[0] == "radius") {
+					radius = atof(nv[1].mb_str());
+				}
+				else if (nv[0] == "ev0") {
+					ev = atof(nv[1].mb_str());
+				}
+				else return false;
+			}
+			else return false;
+		}
+		expv = false;
+	}
+
+	
 	((wxFrame*) m_display->GetParent())->SetStatusText(wxString::Format("exposure compensation %2.2f...", ev));
 	bool result = true;
 	
@@ -168,8 +227,14 @@ bool PicProcessorExposure::processPic(bool processnext)
 	dib = new gImage(getPreviousPicProcessor()->getProcessedPic());
 
 	if (processingenabled) {
+
 		mark();
-		dib->ApplyExposureCompensation(ev, threadcount);
+		if (expv) {
+			dib->ApplyExposureCompensation(ev, threadcount);
+		}
+		else {
+			dib->ApplyExposureCompensation(x, y, radius, ev, threadcount);
+		}
 		wxString d = duration();
 
 		if ((myConfig::getConfig().getValueOrDefault("tool.all.log","0") == "1") || (myConfig::getConfig().getValueOrDefault("tool.exposure.log","0") == "1"))
