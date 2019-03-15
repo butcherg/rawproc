6a04319815221f6302f0be7740ae4ee41178f41a
Glenn Butcher <glenn.butcher@gmail.com>
2019-03-14 22:21:16 -0600

    initial ocio implementation

--- src/PicProcessorOCIO.cpp
+++ src/PicProcessorOCIO.cpp
@@ -1,109 +1,115 @@
-#include "PicProcessor.h"
 #include "PicProcessorOCIO.h"
 #include "PicProcPanel.h"
-#include "myConfig.h"
-#include "undo.xpm"
 #include "util.h"
-
-#include <vector>
-
-#define SHARPENENABLE 7800
+#include "myConfig.h"
+#include <OpenColorIO/OpenColorIO.h>
+namespace OCIO = OCIO_NAMESPACE;
 
 class OCIOPanel: public PicProcPanel
 {
+
 	public:
 		OCIOPanel(wxWindow *parent, PicProcessor *proc, wxString params): PicProcPanel(parent, proc, params)
 		{
-			SetSize(parent->GetSize());
-			wxSizerFlags flags = wxSizerFlags().Center().Border(wxLEFT|wxRIGHT|wxTOP|wxBOTTOM);
-			wxGridBagSizer *g = new wxGridBagSizer();
+			wxBoxSizer *b = new wxBoxSizer(wxVERTICAL);
+			wxBoxSizer *s = new wxBoxSizer(wxHORIZONTAL); 
+			wxSizerFlags flags = wxSizerFlags().Left().Border(wxLEFT|wxRIGHT|wxTOP);
+			//wxArrayString parms = split(params, ",");
+			b->Add(new wxStaticText(this,-1, "ociotransform", wxDefaultPosition, wxSize(100,20)), flags);
+			edit = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(200,25),wxTE_PROCESS_ENTER);
+			b->Add(edit, flags);
+			b->Add(new wxButton(this, wxID_ANY, "Select profile"), flags);
+			b->AddSpacer(10);
 
-			int initialvalue = atoi(params.c_str());
+			wxArrayString opers;
+			opers.Add("convert");
+			opers.Add("assign");
 
-			enablebox = new wxCheckBox(this, SHARPENENABLE, "ocio:");
-			enablebox->SetValue(true);
-			g->Add(enablebox, wxGBPosition(0,0), wxGBSpan(1,3), wxALIGN_LEFT | wxALL, 3);
-			g->Add(new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxSize(280,2)),  wxGBPosition(1,0), wxGBSpan(1,4), wxALIGN_LEFT | wxBOTTOM | wxEXPAND, 10);
-
-			sharp = new wxSlider(this, wxID_ANY, initialvalue, 0, 10, wxPoint(10, 30), wxSize(140, -1));
-			g->Add(sharp , wxGBPosition(2,1), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			val = new wxStaticText(this,wxID_ANY, params, wxDefaultPosition, wxSize(30, -1));
-			g->Add(val , wxGBPosition(2,2), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-			btn = new wxBitmapButton(this, wxID_ANY, wxBitmap(undo_xpm), wxPoint(0,0), wxSize(-1,-1), wxBU_EXACTFIT);
-			btn->SetToolTip("Reset to default");
-			g->Add(btn, wxGBPosition(2,3), wxDefaultSpan, wxALIGN_LEFT | wxALL, 3);
-
-			SetSizerAndFit(g);
-			g->Layout();
+			operselect = new wxRadioBox (this, wxID_ANY, "Operation", wxDefaultPosition, wxDefaultSize,  opers, 1, wxRA_SPECIFY_COLS);
+			//if (parms[1] != "-") operselect->SetSelection(operselect->FindString(parms[1]));
+			s->Add(operselect,flags);
+			
+			wxArrayString intents;
+			intents.Add("perceptual");
+			intents.Add("saturation");
+			intents.Add("relative_colorimetric");
+			intents.Add("absolute_colorimetric");
+			
+			intentselect = new wxRadioBox (this, wxID_ANY, "Rendering Intent", wxDefaultPosition, wxDefaultSize,  intents, 1, wxRA_SPECIFY_COLS);
+			//if (parms[2] != "-") intentselect->SetSelection(intentselect->FindString(parms[2]));
+			s->Add(intentselect,flags);
+			
+			b->Add(s,flags);
+			
+			bpc = new wxCheckBox(this, wxID_ANY, "black point compensation");
+			b->Add(bpc , flags);
+			//bpc->SetValue(bpc);
+			
+			SetSizerAndFit(b);
+			b->Layout();
 			Refresh();
 			Update();
 			SetFocus();
-			t = new wxTimer(this);
-			Bind(wxEVT_BUTTON, &OCIOPanel::OnButton, this);
-			Bind(wxEVT_SCROLL_CHANGED, &OCIOPanel::OnChanged, this);
-			Bind(wxEVT_SCROLL_THUMBTRACK, &OCIOPanel::OnThumbTrack, this);
-			Bind(wxEVT_CHECKBOX, &OCIOPanel::onEnable, this, SHARPENENABLE);
-			Bind(wxEVT_TIMER, &OCIOPanel::OnTimer,  this);
+			Bind(wxEVT_TEXT_ENTER,&OCIOPanel::paramChanged, this);
+			Bind(wxEVT_BUTTON, &OCIOPanel::selectProfile, this);
+			Bind(wxEVT_RADIOBOX,&OCIOPanel::paramChanged, this);
+			Bind(wxEVT_CHECKBOX, &OCIOPanel::paramChanged, this);
 		}
 
 		~OCIOPanel()
 		{
-			t->~wxTimer();
+			//if (s) s->~wxBoxSizer();
 		}
-
-		void onEnable(wxCommandEvent& event)
+		
+		void selectProfile(wxCommandEvent& event)
 		{
-			if (enablebox->GetValue()) {
-				q->enableProcessing(true);
+			wxFileName fname, pname;
+			pname.AssignDir(wxString(myConfig::getConfig().getValueOrDefault("cms.profilepath","").c_str()));
+#ifdef WIN32
+			pname.SetVolume(pname.GetVolume().MakeUpper());
+#endif
+			fname.Assign(wxFileSelector("Select profile", pname.GetPath()));
+			
+			wxString operstr = operselect->GetString(operselect->GetSelection());
+			wxString intentstr = intentselect->GetString(intentselect->GetSelection());
+			if (bpc->GetValue()) intentstr.Append(",bpc");
+
+			if (fname.FileExists()) {
+				edit->SetValue(fname.GetFullName());
+				if (pname.GetPath() == fname.GetPath())
+					q->setParams(wxString::Format("%s,%s,%s",fname.GetFullName(), operstr,intentstr));
+				else
+					q->setParams(wxString::Format("%s,%s,%s",fname.GetFullPath(), operstr,intentstr));					
 				q->processPic();
 			}
-			else {
-				q->enableProcessing(false);
-				q->processPic();
-			}
-		}
-
-		void OnChanged(wxCommandEvent& event)
-		{
-			val->SetLabel(wxString::Format("%4d", sharp->GetValue()));
-			t->Start(500,wxTIMER_ONE_SHOT);
-		}
-
-		void OnThumbTrack(wxCommandEvent& event)
-		{
-			val->SetLabel(wxString::Format("%4d", sharp->GetValue()));
-		}
-
-		void OnTimer(wxTimerEvent& event)
-		{
-			q->setParams(wxString::Format("%d",sharp->GetValue()));
-			q->processPic();
 			event.Skip();
 		}
 
-		void OnButton(wxCommandEvent& event)
+
+		void paramChanged(wxCommandEvent& event)
 		{
-			int resetval = atoi(myConfig::getConfig().getValueOrDefault("tool.sharpen.initialvalue","0").c_str());
-			sharp->SetValue(resetval);
-			q->setParams(wxString::Format("%d",resetval));
-			val->SetLabel(wxString::Format("%4d", resetval));
-			q->processPic();
+			wxString profilestr = edit->GetLineText(0);
+			wxString operstr = operselect->GetString(operselect->GetSelection());
+			wxString intentstr = intentselect->GetString(intentselect->GetSelection());
+			if (bpc->GetValue()) intentstr.Append(",bpc");
+
+			if (profilestr != "(none)") {
+				q->setParams(wxString::Format("%s,%s,%s",profilestr, operstr, intentstr));
+				q->processPic();
+			}
 			event.Skip();
 		}
 
 
 	private:
-		wxSlider *sharp;
-		wxStaticText *val;
-		wxBitmapButton *btn;
-		wxCheckBox *enablebox;
-		wxTimer *t;
-
+		//wxBoxSizer *s;
+		wxCheckBox *bpc;
+		wxTextCtrl *edit;
+		wxRadioBox *operselect, *intentselect;
 
 };
 
-
-PicProcessorOCIO::PicProcessorOCIO(wxString name, wxString command, wxTreeCtrl *tree, PicPanel *display): PicProcessor(name, command,  tree, display) 
+PicProcessorOCIO::PicProcessorOCIO(wxString name, wxString command, wxTreeCtrl *tree, PicPanel *display): PicProcessor(name, command, tree, display) 
 {
 	//showParams();
 }
@@ -116,53 +122,76 @@ void PicProcessorOCIO::createPanel(wxSimplebook* parent)
 	toolpanel->Update();
 }
 
-bool PicProcessorOCIO::processPic(bool processnext) {
-	double kernel[3][3] =
-	{
-		0.0, 0.0, 0.0,
-		0.0, 0.0, 0.0,
-		0.0, 0.0, 0.0
-	};
-
-	double sharp = atof(c.c_str())+1.0;
-	double x = -((sharp-1)/4.0);
-	kernel[0][1] = x;
-	kernel[1][0] = x;
-	kernel[1][2] = x;
-	kernel[2][1] = x;
-	kernel[1][1] = sharp;
+bool PicProcessorOCIO::processPic(bool processnext) 
+{
+	((wxFrame*) m_display->GetParent())->SetStatusText("ocio...");
 	bool result = true;
+	GIMAGE_ERROR ret;
+	
+	wxArrayString cp = split(getParams(),",");
 
-	int threadcount =  atoi(myConfig::getConfig().getValueOrDefault("tool.sharpen.cores","0").c_str());
-
+	int threadcount =  atoi(myConfig::getConfig().getValueOrDefault("tool.ocio.cores","0").c_str());
 	if (threadcount == 0) 
 		threadcount = gImage::ThreadCount();
 	else if (threadcount < 0) 
 		threadcount = std::max(gImage::ThreadCount() + threadcount,0);
 
-	((wxFrame*) m_display->GetParent())->SetStatusText(wxString::Format("sharpen..."));
-
-
+	mark();
 	if (dib) delete dib;
 	dib = new gImage(getPreviousPicProcessor()->getProcessedPic());
-	if (processingenabled & sharp > 1.0) {
-		mark();
-		dib->ApplyConvolutionKernel(kernel, threadcount);
-		wxString d = duration();
-
-		if ((myConfig::getConfig().getValueOrDefault("tool.all.log","0") == "1") || (myConfig::getConfig().getValueOrDefault("tool.sharpen.log","0") == "1"))
-			log(wxString::Format("tool=sharpen,imagesize=%dx%d,threads=%d,time=%s",dib->getWidth(), dib->getHeight(),threadcount,d));
-
-	}
-
-	dirty = false;
 	
+	//do OCIO transform on in-place image
+	try
+	{
+		// Get the global OpenColorIO config
+		// This will auto-initialize (using $OCIO) on first use
+		//OCIO::ConstConfigRcPtr config = OCIO::GetCurrentConfig();
+	
+		OCIO::ConstConfigRcPtr config = OCIO::Config::Create();
+		std::string configfile = myConfig::getConfig().getValueOrDefault("tool.ocio.configpath","");
+		configfile.append(myConfig::getConfig().getValue("tool.ocio.config"));
+		config = OCIO::Config::CreateFromFile(configfile.c_str());
+
+		// Get the processor corresponding to this transform.
+		OCIO::ConstProcessorRcPtr processor = config->getProcessor(OCIO::ROLE_SCENE_LINEAR,
+                                                               OCIO::ROLE_DEFAULT);
+
+		// Wrap the image in a light-weight ImageDescription
+		//OCIO::PackedImageDesc img((float *)dib->getImageDataRaw(), dib->getWidth(), dib->getHeight(), 3);
+
+		// Apply the color transformation (in place)
+		//processor->apply(img);
+		
+		unsigned w = dib->getWidth();
+		unsigned h = dib->getHeight();
+		pix * image = dib->getImageDataRaw();
+		
+		#pragma omp parallel for num_threads(threadcount)
+		for (unsigned y=0; y<h; y++) {
+			unsigned pos = y*w;
+			float * im = (float *) &image[pos];
+			OCIO::PackedImageDesc img(im, w, 1, 3);
+			processor->apply(img);
+		}
+		
+	}
+	catch(OCIO::Exception & exception)
+	{
+		wxMessageBox(wxString::Format("OCIO Exception: %s", wxString(exception.what())));
+	}
+	
+	wxString d = duration();
+
+	if ((myConfig::getConfig().getValueOrDefault("tool.all.log","0") == "1") || (myConfig::getConfig().getValueOrDefault("tool.ocio.log","0") == "1"))
+		log(wxString::Format("tool=ocio,imagesize=%dx%d,time=%s",dib->getWidth(), dib->getHeight(),d));
+	
+	dirty = false;
+
 	((wxFrame*) m_display->GetParent())->SetStatusText("");
 	if (processnext) processNext();
-
+	
 	return result;
 }
-
 
 
 
