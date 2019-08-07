bde3fd1e46c268be71da6f450800038afcb0cac5
Glenn Butcher <glenn.butcher@gmail.com>
2019-08-07 16:44:50 -0600

    fix: oriention constraint to post-demosaic, raw images only (allow JPEG, TIFF, PNG with no Orientation EXIF), crop

--- src/rawprocFrm.cpp
+++ src/rawprocFrm.cpp
@@ -1720,8 +1720,8 @@ void rawprocFrm::MnuCropClick(wxCommandEvent& event)
 {
 	if (commandtree->IsEmpty()) return;
 	gImage dib = ((PicProcessor *) commandtree->GetItemData(commandtree->GetSelection()))->getProcessedPic();
-	if (dib.getInfoValue("Orientation") != "1") {
-		wxMessageBox("Crop tool isn't designed to work work if the image orientation isn't normalized (prior to demosaic?).  Put it in the tool chain after Orientation=1");
+	if (dib.getInfoValue("LibrawMosaiced") == "1" && dib.getInfoValue("Orientation") != "1") {
+		wxMessageBox("Crop tool isn't designed to work work if the image orientation isn't normalized prior to demosaic.  Put it in the tool chain after Orientation=1");
 		return;
 	}
 	SetStatusText("");
