7f7673d866e649aefe9cf76050d93d13d8f19b86
Glenn Butcher <glenn.butcher@gmail.com>
2020-01-01 18:54:05 -0700

    fix: bwpoint data behavior

--- src/PicProcessorBlackWhitePoint.cpp
+++ src/PicProcessorBlackWhitePoint.cpp
@@ -329,13 +329,48 @@ class BlackWhitePointPanel: public PicProcPanel
 		
 		void channelChanged(wxCommandEvent& event)
 		{
-			q->setParams(wxString::Format("%s,%d,%d",chan->GetString(chan->GetSelection()),0,255));
-			((PicProcessorBlackWhitePoint *) q)->setChannel(chan->GetString(chan->GetSelection()));
-			
-			((PicProcessorBlackWhitePoint *) q)->reCalc();
-			updateSliders();
-			processBW();
-			event.Skip();
+			gImage &img = q->getPreviousPicProcessor()->getProcessedPic();
+			std::map<std::string,float> s;
+			double datblk, datwht, minwht;
+			wxString chanstr = chan->GetString(chan->GetSelection());
+			switch (bwmode) {
+				case BLACKWHITESLIDERAUTO:
+					q->setParams(wxString::Format("%s,%d,%d",chan->GetString(chan->GetSelection()),0,255));
+					((PicProcessorBlackWhitePoint *) q)->setChannel(chan->GetString(chan->GetSelection()));
+					((PicProcessorBlackWhitePoint *) q)->reCalc();
+					updateSliders();
+					processBW();
+					break;
+				case BLACKWHITEDATA:
+					s = img.StatsMap();
+					if (chanstr =="rgb" | chanstr == "tone") {
+						datblk = fmin(fmin(s["rmin"],s["gmin"]),s["bmin"]);
+						datwht = fmax(fmax(s["rmax"],s["gmax"]),s["bmax"]);
+						minwht = fmin(fmin(s["rmax"],s["gmax"]),s["bmax"]);
+					}
+					else if (chanstr =="red") {
+						datblk = s["rmin"];
+						datwht = s["rmax"];
+						minwht = s["rmax"];
+					}
+					else if (chanstr =="green") {
+						datblk = s["gmin"];
+						datwht = s["gmax"];
+						minwht = s["gmax"];
+					}
+					else if (chanstr =="blue") {
+						datblk = s["bmin"];
+						datwht = s["bmax"];
+						minwht = s["bmax"];
+					}
+					updateVals(datblk, datwht, minwht);
+					if (minwhite->GetValue())
+						q->setParams(wxString::Format("%s,data,minwhite",chan->GetString(chan->GetSelection())));
+					else
+						q->setParams(wxString::Format("%s,data",chan->GetString(chan->GetSelection())));
+					break;
+			}
+				
 		}
 
 		void OnButton(wxCommandEvent& event)
@@ -479,7 +514,7 @@ bool PicProcessorBlackWhitePoint::processPicture(gImage *processdib)
 					wht = maxwht;
 			}
 			//if (blk < 0.0) blk = 0.0;
-			((BlackWhitePointPanel *) toolpanel)->updateVals(blk,wht,minwht);
+			//((BlackWhitePointPanel *) toolpanel)->updateVals(blk,wht,minwht);
 		}
 
 		else {
